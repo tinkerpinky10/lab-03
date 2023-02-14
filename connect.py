28191a027fa7655731e62d6e59100dab0e5820da
Karla Vasquez <89766689+tinkerpinky10@users.noreply.github.com>
2023-02-13 16:39:48 -0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,13 +1,12 @@
-#connect to environment using requests libraries
 import requests
-
+import lorem
 #connect to another production API
 
 def generate(text):
 
    #pass url to which connection needs to be made into get()
    response_API = "https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud"
-
+   API_KEY= "textvis-word-cloud-v1.p.rapidapi.com"
      # Define the API parameters
    params = {
         "text": text,
@@ -23,8 +22,10 @@ def generate(text):
       data = response.json()
       print("Word cloud image URL: ", data["https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud"])
    else:
-      print("failed", reponse.status_code)
+      print("failed", response.status_code)
 
    text = "text data for generating"
-   generate(text)
    print(response_API.status_code)
+
+if __name__ == '__main__':
+    generate(lorem.paragraph())
